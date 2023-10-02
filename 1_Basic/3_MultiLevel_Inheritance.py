9137fa3ac194feab81becf897b3203acae3a277c
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2023-10-02 20:02:19 +0600

    Update 3_MultiLevel_Inheritance.py

--- 1_Basic/3_MultiLevel_Inheritance.py
+++ 1_Basic/3_MultiLevel_Inheritance.py
@@ -20,26 +20,32 @@ ami.property()
 
 
 
+#Example-02
 
 
-
-
-""" 
 class Grandparent:
-    def __init__(self,name) -> None:
-        self.name = name
+    def __init__(self) -> None:
+        pass
     def property(self):
-        print(self.name," have 5 corer Taka")
+        print("I have 5 corer Taka")
 
 class Parent(Grandparent):
-    def __init__(self,name) -> None:
-        super().__init__(name)
+    pass
     def property(self):
-        print(self.name," have 2 corer Taka")
+        print("I have 3 corer Taka") 
+    def father_property(self):
+        super().property()  
 
 class Child(Parent):
-    def __init__(self,name) -> None:
-        super().__init__(name)
+    pass
     def property(self):
-        print(self.name," have 1 corer Taka")
- """
+        print("I have 2 corer Taka")
+    def father_property(self):
+        super().property()
+    def gfather_property(self):
+        super().father_property()
+
+ami = Child()
+ami.property()
+ami.father_property()
+ami.gfather_property()
