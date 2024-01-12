f1c0724c97f1a9c27f68375624e5990f8bf1851c
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2024-01-12 20:02:00 +0600

    Update 3_MultiLevel_Inheritance.py

--- 1_Basic/3_MultiLevel_Inheritance.py
+++ 1_Basic/3_MultiLevel_Inheritance.py
@@ -21,7 +21,11 @@ ami.property()
 
 
 #Example-02
-
+# GrandFather - 5 crore
+#       |
+# Father - 3 crore
+#       |
+# Ami - 2 crore
 
 class Grandparent:
     def __init__(self) -> None:
@@ -49,3 +53,50 @@ ami = Child()
 ami.property()
 ami.father_property()
 ami.gfather_property()
+
+========================================================================================
+#C++
+#include <iostream>
+using namespace std;
+
+class GrandFather {
+public:
+    void property() {
+        cout << "I have 5 crore taka" << endl;
+    }
+};
+
+class Father : public GrandFather {
+public:
+    void property() {
+        cout << "I have 3 crore taka" << endl;
+    }
+
+    void father() {
+        GrandFather::property();
+    }
+};
+
+class Child : public Father {
+public:
+    void property() {
+        cout << "I have 2 crore taka" << endl;
+    }
+
+    void father() {
+        Father::property();
+    }
+
+    void grandFather() {
+        Father::father();
+    }
+};
+
+int main() {
+    Child ami;
+    ami.property();
+    ami.father();
+    ami.grandFather();
+
+    return 0;
+}
