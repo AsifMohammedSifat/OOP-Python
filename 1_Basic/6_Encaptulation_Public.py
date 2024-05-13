8d11390d8b45b30ac0cb5cc1ea301e30932b8dac
Asif Mohammed Sifat <asifmohammedsifat38@gmail.com>
2024-05-13 19:48:18 +0600

    Update 6_Encaptulation_Public.py

--- 1_Basic/6_Encaptulation_Public.py
+++ 1_Basic/6_Encaptulation_Public.py
@@ -22,3 +22,39 @@ print("After Re-assign")
 obStudent.id = -30 #reassign and dont show any error
 
 obStudent.details()
+
+
+
+======
+C++:
+======
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student{
+    
+    public:
+    string name;
+    int id;
+
+    Student(string name,int id){
+        this->name = name;
+        this->id = id;     
+    }
+    void details(){
+        cout<<"Name = "<<name<<" "<<"ID = "<<id<<endl;
+    }
+};
+
+int main()
+{
+    Student obj("Avi",5);
+    obj.details();
+    obj.id = 1;
+    obj.name = "Akash";
+    obj.details();
+
+
+    return 0;
+}
